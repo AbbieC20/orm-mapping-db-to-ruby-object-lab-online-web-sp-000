6d4a217f398ae981d70ec05dca209ed252c4798a
remote-torrent-6747 <abbyc@sky.com>
2021-03-14 16:31:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -108,6 +108,19 @@ class Student
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
+    # returns the first element rather than the array!
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
   end
 
 
